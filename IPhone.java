0ef1cebc82be5e9fca47baaef27e6026d0803c6a
GiselaSoares <giselacss16@gmail.com>
2023-09-26 12:18:34 -0300

    Commit desafio poo

---
+++ IPhone.java
@@ -0,0 +1,39 @@
+public class IPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    private String estado;
+
+    public void tocar() {
+        // Implementação do método tocar
+    }
+
+    public void pausar() {
+        // Implementação do método pausar
+    }
+
+    public void selecionarMusica() {
+        // Implementação do método selecionarMusica
+    }
+
+    public void ligar(String numero) {
+        // Implementação do método ligar
+    }
+
+    public void atender() {
+        // Implementação do método atender
+    }
+
+    public void iniciarCorreioVoz() {
+        // Implementação do método iniciarCorreioVoz
+    }
+
+    public void exibirPagina(String url) {
+        // Implementação do método exibirPagina
+    }
+
+    public void adicionarNovaAba() {
+        // Implementação do método adicionarNovaAba
+    }
+
+    public void atualizarPagina() {
+        // Implementação do método atualizarPagina
+    }
+}
